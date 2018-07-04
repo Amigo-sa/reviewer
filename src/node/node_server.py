5d5e4eacbbb01f2924a54b6d5ff218dfad82b522
kir <kirdark@inbox.ru>
2018-07-04 22:31:25 +0300

    Добавил задержку при попытке регистрации Вынес опрос сервера в отдельный поток

--- src/node/node_server.py
+++ src/node/node_server.py
@@ -1,9 +1,11 @@
 import requests
 import time
+from threading import Thread
 
 me = {"ip":"10.0.0.10", "port": 1000}
 status = False
 while not status:
+    time.sleep(2)
     try:
         r = requests.post('http://127.0.0.1:5000/register', json = me)
         if r.status_code == requests.codes.ok:
@@ -13,11 +15,21 @@ while not status:
 
 
 existing_nodes = None
-while True:
-    time.sleep(2)
-    try:
-        r = requests.get('http://127.0.0.1:5000/get_network_info')
-        existing_nodes = r.json()
-        print(existing_nodes)
-    except:
-        print("Сервер не доступен")
+
+class UpdateNetwork(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+
+    def run(self):
+        while True:
+            time.sleep(2)
+            try:
+                r = requests.get('http://127.0.0.1:5000/get_network_info')
+                existing_nodes = r.json()
+                print(existing_nodes)
+            except:
+                print("Сервер не доступен")
+
+update_network_thread = UpdateNetwork()
+update_network_thread.start()
+print("Script ended")
