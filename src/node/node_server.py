14b93f6f585a8c68426dcff5a074a3b8815123f2
kir <kirdark@inbox.ru>
2018-07-06 21:47:22 +0300

    Сделал проверку на существование базы и вывод чего-нибуть из нее

--- src/node/node_server.py
+++ src/node/node_server.py
@@ -1,13 +1,48 @@
 import requests
 import time
 from threading import Thread
-import settings.constants as CONSTANTS
+import settings.constants as constants
+import pymongo
+
+
+def is_db_exists():
+    rev_client = pymongo.MongoClient(constants.mongo_db)
+    rev_db = rev_client["reviewer"]
+
+    try:
+        col_list = rev_db.list_collection_names()
+    except Exception as ex:
+        print(ex)
+        return False
+    if col_list == []:
+        return False
+    return True
+
+
+def print_something_from_db():
+
+    rev_client = pymongo.MongoClient(constants.mongo_db)
+    rev_db = rev_client["reviewer"]
+    col_person = rev_db["Person"]
+    cursor = col_person.find({})
+
+    try:
+        for person in cursor:
+            print(person["Name"])
+    except Exception as ex:
+        print(ex)
+
+    return
+
+
+if (is_db_exists()):
+    print_something_from_db()
 
 status = False
 while not status:
     time.sleep(2)
     try:
-        r = requests.post(CONSTANTS.core_server_url +'/register', json = CONSTANTS.node_id)
+        r = requests.post(constants.core_server_url +'/register', json = constants.node_id)
         if r.status_code == requests.codes.ok:
             status = True
     except Exception as ex:
@@ -24,7 +59,7 @@ class UpdateNetwork(Thread):
         while True:
             time.sleep(2)
             try:
-                r = requests.get(CONSTANTS.core_server_url +'/get_network_info')
+                r = requests.get(constants.core_server_url +'/get_network_info')
                 existing_nodes = r.json()
                 print(existing_nodes)
             except Exception as ex:
@@ -32,4 +67,4 @@ class UpdateNetwork(Thread):
 
 update_network_thread = UpdateNetwork()
 update_network_thread.start()
-print("Script ended")
+
--- src/node/settings/constants.py
+++ src/node/settings/constants.py
@@ -1,2 +1,3 @@
 node_id = {"ip":"10.0.0.10", "port": 1000}
 core_server_url = "http://127.0.0.1:5000"
+mongo_db = "mongodb://localhost:27017"
