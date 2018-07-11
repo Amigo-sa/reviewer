6694446825d1b6bf8ba9af3f53263075ad22af6e
kir <kirdark@inbox.ru>
2018-07-11 22:54:08 +0300

    Родил первую функцию. Пока без проверки на корректность входных данных

--- src/node/api/client_api.py
+++ src/node/api/client_api.py
@@ -1,6 +1,6 @@
 import settings.constants as constants
 import pymongo
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
 
 rev_client = pymongo.MongoClient(constants.mongo_db)
 rev_db = rev_client["reviewer"]
@@ -20,3 +20,15 @@ if __debug__:
             for document in cursor:
                   result_string += "document: {0}<br>".format(document)
         return result_string
+
+@bp.route("/add_organization", methods = ['POST'])
+def add_organization():
+    req = request.get_json()
+    cursor = rev_db['organization']
+    item = {"name": req['name']}
+    id = cursor.insert(item)
+    result = {"result":0,
+              "id": str(id)}
+
+    return jsonify(result), 200
+
