5a67595985851f6a170c37aa04cdb8637266954f
Alexander Ignatov <stross-1@ya.ru>
2018-07-20 11:08:59 +0300

    Добавлен тест на добавление и удаление организации

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -1,5 +1,4 @@
 import context
-from data.reviewer_model import *
 import unittest
 import requests
 from src.node.settings import constants
@@ -15,12 +14,39 @@ class TestApi(unittest.TestCase):
         self.assertEqual(200, req.status_code)
         resp = req.json()
         self.assertEqual(resp["result"], ERR.OK)
-        # TODO заменить на аггрегацию
-        found_mpei = False
+        self.assertTrue(self.find_org(resp,'МЭИ'))
+
+    def test_add_del_organization_normal(self):
+        resp = requests.get(self.api_URL + "/organizations").json()
+        self.assertEqual(resp["result"], ERR.OK)
+
+        mephi_id = self.find_org(resp, 'МИФИ')
+        if mephi_id:
+            resp = requests.delete(self.api_URL + "/organizations/" + str(mephi_id)).json()
+            self.assertEqual(resp['result'], ERR.OK)
+
+        post_data = {'name' : 'МИФИ'}
+        resp = requests.post(url=self.api_URL + '/organizations',json = post_data).json()
+        self.assertEqual(resp['result'], ERR.OK)
+
+        resp = requests.get(self.api_URL + '/organizations').json()
+        mephi_id = self.find_org(resp, 'МИФИ')
+        self.assertTrue(mephi_id)
+
+        resp = requests.delete(self.api_URL + "/organizations/" + str(mephi_id)).json()
+        self.assertEqual(resp['result'], ERR.OK)
+
+        resp = requests.get(self.api_URL + '/organizations').json()
+        mephi_id = self.find_org(resp, 'МИФИ')
+        self.assertFalse(mephi_id)
+
+
+    @staticmethod
+    def find_org(resp, name):
         for item in resp['list']:
-            if item['name'] == 'МЭИ':
-                found_mpei = True
-        self.assertTrue(found_mpei)
+            if item['name'] == name:
+                return item['id']
+
 
 if __name__ == "__main__":
     unittest.main(verbosity = 1)
