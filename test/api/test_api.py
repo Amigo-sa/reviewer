775c5f96dd4ee0172e07601ee0c789bd64bd3f7c
Alexander Ignatov <stross-1@ya.ru>
2018-07-21 16:35:49 +0300

    Добавлен "нормальный" тест для департамента

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -34,9 +34,9 @@ class TestApi(unittest.TestCase):
         pass
         requests.post(cls.api_URL + "/shutdown")
 
-    def t_not_referencing_normal(self, access_url, *args, **kwargs):
+    def t_simple_normal(self, url_get, url_post, url_delete, *args, **kwargs):
         # read from empty DB
-        resp = requests.get(self.api_URL + access_url)
+        resp = requests.get(self.api_URL + url_get)
         self.assertEqual(200, resp.status_code, "get response status code must be 200")
         resp_json = resp.json()
         self.assertEqual(resp_json["result"], ERR.OK, "result must be ERR.OK")
@@ -67,16 +67,16 @@ class TestApi(unittest.TestCase):
                     })
                 else:
                     raise ValueError("unsupported field type")
-            resp = requests.post(url=self.api_URL + access_url, json=cur_item)
+            resp = requests.post(url=self.api_URL + url_post, json=cur_item)
             self.assertEqual(200, resp.status_code, "post response status code must be 200")
             resp_json = resp.json()
             self.assertEqual(resp_json["result"], ERR.OK, "post result must be ERR.OK")
             self.assertTrue(resp_json["id"], "returned id must be not None")
             cur_item.update({"id" : resp_json["id"]})
             add_list.append(cur_item)
-        print("sample data for " + access_url + ":\n" + str(add_list))
+        print("sample data for " + url_post + ":\n" + str(add_list))
         # verify written items
-        resp_json = requests.get(self.api_URL + access_url).json()
+        resp_json = requests.get(self.api_URL + url_get).json()
         read_list = resp_json["list"]
         total_match = 0
         for added_item in add_list:
@@ -89,40 +89,78 @@ class TestApi(unittest.TestCase):
         self.assertEqual(total_match, items_to_write, "all added items must be in GET list")
         # delete items
         for item in add_list:
-            resp = requests.delete(url=self.api_URL + access_url + "/" + item["id"])
+            resp = requests.delete(url=self.api_URL + url_delete + "/" + item["id"])
             self.assertEqual(200, resp.status_code, "delete response status code must be 200")
             resp_json = resp.json()
             self.assertEqual(resp_json["result"], ERR.OK, "result must be ERR.OK")
         # verify deletion
-        resp_json = requests.get(self.api_URL + access_url).json()
+        resp_json = requests.get(self.api_URL + url_get).json()
         read_list = resp_json["list"]
         self.assertListEqual([], read_list, "docs must be erased from DB")
 
+    def t_referencing_normal(self):
+        pass
+
+    def prepare_organization(self):
+        post_data = {"name": "aux_org"}
+        resp_json = requests.post(url=self.api_URL + '/organizations', json=post_data).json()
+        if resp_json["result"] == ERR.OK:
+            return resp_json["id"]
+        # return None
+
+    def delete_organization(self, id):
+        resp_json = requests.delete(url=self.api_URL + '/organizations' + "/" + id).json()
+        self.assertEqual(resp_json["result"], ERR.OK, "the organization must be deleted")
 
     def test_organization_normal(self):
-        self.t_not_referencing_normal("/organizations", name = "string")
+        self.t_simple_normal(         "/organizations",
+                                      "/organizations",
+                                      "/organizations",
+                                        name = "string")
 
     def test_person_normal(self):
-        self.t_not_referencing_normal("/persons",
-                                      first_name="string",
-                                      middle_name="string",
-                                      surname="string",
-                                      birth_date="date",
-                                      phone_no = "number_string"
-                                      )
+        self.t_simple_normal("/persons",
+                            "/persons",
+                            "/persons",
+                             first_name="string",
+                             middle_name="string",
+                             surname="string",
+                             birth_date="date",
+                             phone_no="number_string"
+                             )
 
     def test_soft_skill_normal(self):
-        self.t_not_referencing_normal("/soft_skills", name="string")
+        self.t_simple_normal("/soft_skills",
+                            "/soft_skills",
+                            "/soft_skills",
+                             name="string")
 
     def test_hard_skill_normal(self):
-        self.t_not_referencing_normal("/hard_skills", name="string")
+        self.t_simple_normal("/hard_skills",
+                            "/hard_skills",
+                            "/hard_skills",
+                             name="string")
 
     def test_group_roles_normal(self):
-        self.t_not_referencing_normal("/group_roles", name="string")
+        self.t_simple_normal("/group_roles",
+                            "/group_roles",
+                            "/group_roles",
+                             name="string")
 
     def test_group_permissions_normal(self):
-        self.t_not_referencing_normal("/group_permissions", name="string")
+        self.t_simple_normal("/group_permissions",
+                             "/group_permissions",
+                             "/group_permissions",
+                             name="string")
 
+    def test_department_normal(self):
+        aux_org_id = self.prepare_organization()
+        self.assertTrue(aux_org_id, "auxiliary organization must be created")
+        self.t_simple_normal(         "/organizations/" + aux_org_id + "/departments",
+                                      "/organizations/" + aux_org_id + "/departments",
+                                      "/departments",
+                                        name="string")
+        self.delete_organization(aux_org_id)
 
 
 if __name__ == "__main__":
