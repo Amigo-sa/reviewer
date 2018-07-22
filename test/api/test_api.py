b6ab851eb0f1531493c2e80a250317b37ff0dcc8
Alexander Ignatov <stross-1@ya.ru>
2018-07-22 18:27:44 +0300

    Добавлены "нормальные" тесты StudentRole и TutorRole

--- test/api/test_api.py
+++ test/api/test_api.py
@@ -31,7 +31,6 @@ class TestApi(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        pass
         requests.post(cls.api_URL + "/shutdown")
 
     def t_simple_normal(self, url_get, url_post, url_delete, *args, **kwargs):
@@ -61,7 +60,7 @@ class TestApi(unittest.TestCase):
         total_match = 0
         for added_item in add_list:
             item_match = 0
-            if "info" in added_item.keys(): del added_item["info"] # Подразумевает, что мы не ожидаем получение info при получении списка, т.к. инфо может быть длинное
+            if "info" in added_item.keys(): del added_item["info"] # Подразумевает, что мы не ожидаем получение "info" при получении списка, т.к. инфо может быть длинное
             for read_item in read_list:
                 if added_item == read_item:
                     item_match += 1
@@ -80,7 +79,7 @@ class TestApi(unittest.TestCase):
         self.assertListEqual([], read_list, "docs must be erased from DB")
 
     @staticmethod
-    def generate_doc(doc_no, type_list):
+    def generate_doc(doc_no, type_list, *args, **kwargs):
         cur_item = dict()
         for key, value in type_list:
             if value == "string":
@@ -108,22 +107,24 @@ class TestApi(unittest.TestCase):
                 cur_item.update({
                     key: value
                 })
+        for key, value in kwargs.items():
+            cur_item.update({key: value})
         return cur_item
 
     def prepare_organization(self):
         post_data = {"name": "aux_org"}
         resp_json = requests.post(url=self.api_URL + '/organizations', json=post_data).json()
-        if resp_json["result"] == ERR.OK:
-            return resp_json["id"]
+        self.assertEqual(resp_json["result"],ERR.OK, "aux organization must be created")
+        return resp_json["id"]
 
     def prepare_department(self):
         aux_org_id = self.prepare_organization()
         self.assertTrue(aux_org_id, "auxiliary organization must be created")
         post_data = {"name": "aux_dep"}
         resp_json = requests.post(url=self.api_URL + '/organizations/' + aux_org_id + "/departments", json=post_data).json()
-        if resp_json["result"] == ERR.OK:
-            return {"dep_id" : resp_json["id"],
-                    "org_id" : aux_org_id}
+        self.assertEqual(resp_json["result"], ERR.OK, "aux department must be created")
+        return {"dep_id" : resp_json["id"],
+                "org_id" : aux_org_id}
 
     def prepare_group(self):
         aux_items_ids = self.prepare_department()
@@ -131,9 +132,9 @@ class TestApi(unittest.TestCase):
         post_data = {"name": "aux_group"}
         resp_json = requests.post(url=self.api_URL + '/departments/' + aux_items_ids["dep_id"] + "/groups",
                                   json=post_data).json()
-        if resp_json["result"] == ERR.OK:
-            aux_items_ids.update({"group_id": resp_json["id"]})
-            return aux_items_ids
+        self.assertEqual(resp_json["result"], ERR.OK, "aux group must be created")
+        aux_items_ids.update({"group_id": resp_json["id"]})
+        return aux_items_ids
 
     def prepare_persons(self, person_count):
         person_type_list = dict(first_name="string",
@@ -142,7 +143,6 @@ class TestApi(unittest.TestCase):
                              birth_date="date",
                              phone_no="number_string")
         id_list = []
-        print(person_type_list)
         for person_ctr in range(person_count):
             cur_person = self.generate_doc(person_ctr, person_type_list.items())
             resp_json = requests.post(url=self.api_URL + "/persons", json=cur_person).json()
@@ -153,8 +153,7 @@ class TestApi(unittest.TestCase):
     def prepare_hs(self):
         post_data = {"name": "aux_hard_skill"}
         resp_json = requests.post(url=self.api_URL + '/hard_skills', json=post_data).json()
-        if resp_json["result"] == ERR.OK:
-            return resp_json["id"]
+        self.assertEqual(resp_json["result"], ERR.OK, "aux hard skill must be created")
 
     def delete_doc(self, url):
         resp_json = requests.delete(url=self.api_URL + url).json()
@@ -203,7 +202,6 @@ class TestApi(unittest.TestCase):
 
     def test_department_normal(self):
         aux_org_id = self.prepare_organization()
-        self.assertTrue(aux_org_id, "auxiliary organization must be created")
         self.t_simple_normal(         "/organizations/" + aux_org_id + "/departments",
                                       "/organizations/" + aux_org_id + "/departments",
                                       "/departments",
@@ -212,7 +210,6 @@ class TestApi(unittest.TestCase):
 
     def test_group_normal(self):
         aux_item_ids = self.prepare_department()
-        self.assertTrue(aux_item_ids, "auxiliary organization must be created")
         self.t_simple_normal("/departments/" + aux_item_ids["dep_id"] + "/groups",
                              "/departments/" + aux_item_ids["dep_id"] + "/groups",
                              "/groups",
@@ -237,17 +234,84 @@ class TestApi(unittest.TestCase):
     """
 
     def test_group_test_normal(self):
-        aux_item_ids = self.prepare_group()
-        self.assertTrue(aux_item_ids["group_id"], "aux group must be created")
-        self.t_simple_normal("/groups/" + aux_item_ids["group_id"] + "/tests",
-                             "/groups/" + aux_item_ids["group_id"] + "/tests",
+        aux_doc_ids = self.prepare_group()
+        self.assertTrue(aux_doc_ids["group_id"], "aux group must be created")
+        self.t_simple_normal("/groups/" + aux_doc_ids["group_id"] + "/tests",
+                             "/groups/" + aux_doc_ids["group_id"] + "/tests",
                              "/tests",
                              name="string",
                              info="string")
-        self.delete_doc("/groups/" + aux_item_ids["group_id"])
-        self.delete_doc("/departments/" + aux_item_ids["dep_id"])
-        self.delete_doc("/organizations/" + aux_item_ids["org_id"])
+        self.delete_doc("/groups/" + aux_doc_ids["group_id"])
+        self.delete_doc("/departments/" + aux_doc_ids["dep_id"])
+        self.delete_doc("/organizations/" + aux_doc_ids["org_id"])
 
+    def test_tutor_role_normal(self):
+        self.t_role("Tutor")
+        pass
+
+    def test_student_role_normal(self):
+        self.t_role("Student")
+        pass
+
+    def t_role(self, role_type):
+        person_count = 2
+        aux_persons_ids = self.prepare_persons(person_count)
+        aux_facilities_ids = self.prepare_department()
+        roles = []
+        for person_id in aux_persons_ids:
+            role_list = self.get_item_list("/persons/%s/general_roles" % person_id)
+            self.assertEqual([], role_list)
+            cur_role = {
+                "person_id": person_id,
+                "department_id": aux_facilities_ids["dep_id"],
+                "role_type": role_type,
+                "description": "sample_description"
+            }
+            cur_role.update({"id": self.post_item("/general_roles", cur_role)})
+            roles.append(cur_role)
+        for role in roles:
+            role_no_id = role.copy()
+            del role_no_id["id"]
+            role_data = self.get_item_data("/general_roles/" + role["id"])
+            self.assertEqual(role_no_id, role_data, "returned general role data must match inserted data")
+        for person_num, person_id in enumerate(aux_persons_ids):
+            role_list = self.get_item_list("/persons/%s/general_roles" % person_id)
+            self.assertEqual([{"id": roles[person_num]["id"]}],
+                             role_list, "role list by person id must return correct data")
+        for role in roles:
+            self.delete_item("/general_roles/" + role["id"])
+        self.delete_doc("/departments/" + aux_facilities_ids["dep_id"])
+        self.delete_doc("/organizations/" + aux_facilities_ids["org_id"])
+        for person_id in aux_persons_ids:
+            self.delete_doc("/persons/" + person_id)
+
+    def get_item_list(self, url):
+        resp = requests.get(self.api_URL + url)
+        self.assertEqual(200, resp.status_code, "get response status code must be 200")
+        resp_json = resp.json()
+        self.assertEqual(resp_json["result"], ERR.OK, "result must be ERR.OK")
+        return resp_json["list"]
+
+    def get_item_data(self, url):
+        resp = requests.get(self.api_URL + url)
+        self.assertEqual(200, resp.status_code, "get response status code must be 200")
+        resp_json = resp.json()
+        self.assertEqual(resp_json["result"], ERR.OK, "result must be ERR.OK")
+        return resp_json["data"]
+
+    def post_item(self, url, data):
+        resp = requests.post(url=self.api_URL + url, json=data)
+        self.assertEqual(200, resp.status_code, "post response status code must be 200")
+        resp_json = resp.json()
+        self.assertEqual(resp_json["result"], ERR.OK, "post result must be ERR.OK")
+        self.assertTrue(resp_json["id"], "returned id must be not None")
+        return resp_json["id"]
+
+    def delete_item(self, url):
+        resp = requests.delete(url=self.api_URL + url)
+        self.assertEqual(200, resp.status_code, "delete response status code must be 200")
+        resp_json = resp.json()
+        self.assertEqual(resp_json["result"], ERR.OK, "result must be ERR.OK")
 
 if __name__ == "__main__":
     unittest.main(verbosity=1)
