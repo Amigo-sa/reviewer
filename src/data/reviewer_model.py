f35b2b86bd4f3352c6ced84ce77d71c2f6c2d532
Alexander Ignatov <stross-1@ya.ru>
2018-07-23 23:31:20 +0300

    add encoding rules

--- src/data/reviewer_model.py
+++ src/data/reviewer_model.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 from pymodm import MongoModel, fields, ReferenceField
 from pymongo.write_concern import WriteConcern
 from pymodm.errors import ValidationError
--- test/api/test_api.py
+++ test/api/test_api.py
@@ -1,3 +1,4 @@
+# -- coding: utf-8 --
 import context
 import unittest
 import requests
