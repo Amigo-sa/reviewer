48f1dc144b2b1d8bf3dc0c9f4922f6453c3b34e4
kir <kirdark@inbox.ru>
2018-09-11 20:51:38 +0300

    add skill type rest api functions

--- src/node/api/skills.py
+++ src/node/api/skills.py
@@ -10,11 +10,34 @@ from node.api.base_functions import delete_resource, list_resources, add_resourc
 bp = Blueprint('skills', __name__)
 
 
-@bp.route("/soft_skills", methods = ['POST'])
+@bp.route("/skill_types", methods = ['POST'])
 @required_auth("admin")
-def add_soft_skill():
-    return add_resource(SoftSkill,
+def add_skill_type():
+    return add_resource(SkillType,
                         ["name"])
+
+
+@bp.route("/skill_types/<string:id>", methods = ['DELETE'])
+@required_auth("admin")
+def delete_skill_type(id):
+    return delete_resource(SkillType, id)
+
+
+@bp.route("/skill_types", methods = ['GET'])
+def list_skill_types():
+    return list_resources(SkillType,
+                          {"id": "_id",
+                           "name": "name"})
+
+
+@bp.route("/skill_types/<string:id>/soft_skills", methods = ['POST'])
+@required_auth("admin")
+def add_soft_skill(id):
+    return add_resource(SoftSkill,
+                        ["name"],
+                        SkillType,
+                        id,
+                        "skill_type_id")
 
 
 @bp.route("/soft_skills/<string:id>", methods = ['DELETE'])
@@ -27,14 +50,18 @@ def delete_soft_skill(id):
 def list_soft_skills():
     return list_resources(SoftSkill,
                           {"id": "_id",
-                           "name": "name"})
+                           "name": "name",
+                           "skill_type": "skill_type_id"})
 
 
-@bp.route("/hard_skills", methods = ['POST'])
+@bp.route("/skill_types/<string:id>/hard_skills", methods = ['POST'])
 @required_auth("admin")
-def add_hard_skill():
+def add_hard_skill(id):
     return add_resource(HardSkill,
-                        ["name"])
+                        ["name"],
+                        SkillType,
+                        id,
+                        "skill_type_id")
 
 
 @bp.route("/hard_skills/<string:id>", methods = ['DELETE'])
@@ -47,4 +74,5 @@ def delete_hard_skill(id):
 def list_hard_skills():
     return list_resources(HardSkill,
                           {"id": "_id",
-                           "name": "name"})
+                           "name": "name",
+                           "skill_type": "skill_type_id"})
