6b4587302fe76ede5fbdd7250033aecdb5380921
ZhilinRodion <zhilin.rodion@gmail.com>
2018-09-21 12:06:42 +0300

    Temp disable service worker.

--- src/web_client/src/index.tsx
+++ src/web_client/src/index.tsx
@@ -4,7 +4,6 @@ import * as ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 import AuthStore from './stores/AuthStore';
 const stores = {
     AuthStore
@@ -18,4 +17,5 @@ ReactDOM.render(
     </Provider>,
     document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
+// TODO: temp disable service worker. There is problem with it on apache server.
+// registerServiceWorker();
