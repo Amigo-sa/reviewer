70219afd07b6dd445f16e55b9fb8b57ee72c8bf6
ZhilinRodion <zhilin.rodion@gmail.com>
2018-09-30 20:48:10 +0300

    Removed test code.

--- src/web_client/src/pages/Main.tsx
+++ src/web_client/src/pages/Main.tsx
@@ -1,17 +1,7 @@
 import * as React from "react";
-import RegistrationApi from "../server-api/registration/RegistrationApi";
-import UserLoginRequest from "../server-api/registration/UserLoginRequest";
-import UserLoginResponse from "../server-api/registration/UserLoginResponse";
 
 class Main extends React.Component {
     public render() {
-
-        RegistrationApi.userLogin(new UserLoginRequest("79032233223", "SomeSecurePass"))
-            .then((response: UserLoginResponse) => {
-                console.log(response.result);
-                console.log(response.session_id);
-            });
-
         return (
             "Main page"
         );
