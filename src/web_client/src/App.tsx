d9e74eecb3b93b90b5f2ff79b7bc4b5aecb98c08
Rodion Zhilin <zhilin.rodion@gmail.com>
2018-10-01 12:47:59 +0300

    Clean up index file.

--- src/web_client/src/App.tsx
+++ src/web_client/src/App.tsx
@@ -10,20 +10,58 @@ import PrivateRoute from "./components/PrivateRoute";
 import LoginPage from "./pages/LoginPage";
 import AuthorizationUIHelper from "./AuthorizationUIHelper";
 import authStore from "./stores/AuthStore";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import HeaderMenu from "./components/Menu";
 
 const authUIHelper = new AuthorizationUIHelper(authStore);
 
 class App extends React.Component<any> {
     public render() {
         return (
-            <Switch>
-                <Route exact={true} path="/" component={Main} />
-                <Route path="/login" component={LoginPage} />
-                <PrivateRoute path="/personal" component={PersonalPage} authHelper={authUIHelper} />
-                <PrivateRoute path="/search-peoples" component={SearchPeoplesPage} authHelper={authUIHelper} />
-                <PrivateRoute path="/search-structures" component={SearchStructuresPage} authHelper={authUIHelper} />
-                <PrivateRoute path="/add-survey" component={AddSurveyPage} authHelper={authUIHelper} />
-            </Switch>
+            <Grid
+                container={true}
+                spacing={24}
+            >
+                <Grid
+                    item={true}
+                    xs={12}
+                >
+                    <HeaderMenu />
+                </Grid>
+                <Grid
+                    item={true}
+                    xs={12}
+                >
+                    <Paper>
+                        <Switch>
+                            <Route exact={true} path="/" component={Main} />
+                            <Route path="/login" component={LoginPage} />
+                            <PrivateRoute
+                                path="/personal"
+                                component={PersonalPage}
+                                authHelper={authUIHelper}
+                            />
+                            <PrivateRoute
+                                path="/search-peoples"
+                                component={SearchPeoplesPage}
+                                authHelper={authUIHelper}
+                            />
+                            <PrivateRoute
+                                path="/search-structures"
+                                component={SearchStructuresPage}
+                                authHelper={authUIHelper}
+                            />
+                            <PrivateRoute
+                                path="/add-survey"
+                                component={AddSurveyPage}
+                                authHelper={authUIHelper}
+                            />
+                        </Switch>
+                    </Paper>
+                </Grid>
+            </Grid>
+
         );
     }
 }
