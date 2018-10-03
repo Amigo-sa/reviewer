bde9b79367600530ab5df1eea76d662b829ff179
Alexander Ignatov <stross-1@ya.ru>
2018-10-03 13:45:59 +0300

    Improve time measuring script

--- test/load/db_performance.py
+++ test/load/db_performance.py
@@ -1,27 +1,157 @@
 # -*- coding: utf-8 -*-
+import gc
 
 import context
 import node.settings.errors as ERR
 import data.reviewer_model as model
+import sample_local as sl
+import datetime
 
 import time
 
-g_qs = model.Group.objects.all()
-g_list = list(g_qs.values())
-print("Number of groups: %d, now finding persons in each"%len(g_list))
+def get_group_list():
+    g_qs = model.Group.objects.all()
+    g_list = list(g_qs.values())
+    return g_list
 
-def eval_find_persons_in_groups(route_name):
+def count_persons():
+    return model.Person.objects.all().count()
+
+def eval_find_persons_in_groups(route_name, g_list, skip):
     t_start = time.time()
     print("%s method: started at %f" % (route_name,t_start))
     for group in g_list:
         person_list = model.Person.find({route_name : group["_id"]
-                                        ,"query_skip" : 500
+                                        ,"query_skip" : skip
                                         ,"query_limit" : 100})
     print("last returned list had %d elements" % len(person_list["list"]))
     t_finish = time.time()
     print("finished at %f" % t_finish)
     dt = t_finish - t_start
     print("elapsed %f" % dt)
+    return dt
 
-eval_find_persons_in_groups("group_id")
-eval_find_persons_in_groups("group_id_mod")
+#eval_find_persons_in_groups("group_id")
+#eval_find_persons_in_groups("group_id_mod")
+
+fill_groups = [
+    {"person" : 100
+    ,"group_member" : 100
+    ,"group" : 10
+    ,"group_role" : 10},
+
+
+
+    {"person" : 1000
+    ,"group_member" : 1000
+    ,"group" : 10
+    ,"group_role" : 10},
+
+    {"person" : 10000
+    ,"group_member" : 10000
+    ,"group" : 10
+    ,"group_role" : 10},
+
+    {"person" : 100000
+    ,"group_member" : 1000000
+    ,"group" : 10
+    ,"group_role" : 10},
+
+    {"person" : 10000
+    ,"group_member" : 100000
+    ,"group" : 10
+    ,"group_role" : 1},
+
+    {"person" : 20010
+    ,"group_member" : 20011
+    ,"group" : 1000
+    ,"group_role" : 10},
+
+    {"person" : 10000,
+    "group_member" : 1000,
+    "group" : 10,
+    "group_role" : 10,
+    "person_ss" : 10000,
+    "person_hs" : 10000,
+    "ss_review" : 20010,
+    "hs_review" : 20010,
+    "specialization_review" : 11,
+    "person_specialization": 2001,},
+
+    {"person" : 10000,
+    "group_member" : 10000,
+    "group" : 10,
+    "group_role" : 1,
+    "person_ss" : 10000,
+    "person_hs" : 10000,
+    "ss_review" : 20010,
+    "hs_review" : 20010,
+    "specialization_review" : 11,
+    "person_specialization": 2001,},
+
+    {"person" : 100
+    ,"group_member" : 100
+    ,"group" : 1000
+    ,"group_role" : 10},
+
+    {"person": 100000
+    ,"group_member": 200000
+    ,"group": 2
+    ,"group_role": 1},
+
+]
+routes_to_test = [
+    "group_id_mod",
+    "group_id",
+]
+
+def prepare_header(fill_groups, routes_to_test):
+    headers = []
+    for group in fill_groups:
+        print(group)
+        for key,value in group.items():
+            if key not in headers:
+                headers.append(key)
+    for route in routes_to_test:
+        headers.append("%s, no skip, limit 100" % route)
+        headers.append("%s, skip half, limit 100" % route)
+    return headers
+
+
+if __name__ == "__main__":
+    filename = "out_{:%Y-%m-%d_%H%M%S}.csv".format(datetime.datetime.now())
+    headers = prepare_header(fill_groups, routes_to_test)
+    f = open(filename, "w")
+    for header in headers:
+        f.write("%s;"%header)
+    f.write("\n")
+    f.close()
+
+    for group in fill_groups:
+
+        with open(filename, "a") as f:
+            sl.clear_collections(sl.db)
+            sl.fill_db(sl.db,group,sl.validate_after_fill,sl.diverse)
+            g_list = get_group_list()
+            p_count = count_persons()
+            avg_persons_per_group = p_count/len(g_list)
+            print("avg persons in group: %s" % avg_persons_per_group)
+            skip = 0 if avg_persons_per_group < 200 else int(avg_persons_per_group/2)
+            for route in routes_to_test:
+                gc.collect()
+                t = eval_find_persons_in_groups(route, g_list, 0)
+                group.update({"%s, no skip, limit 100" % route : int(t*1000)})
+                if skip:
+                    gc.collect()
+                    t = eval_find_persons_in_groups(route, g_list, skip)
+                    group.update({"%s, skip half, limit 100" % route: int(t * 1000)})
+                else:
+                    group.update({"%s, skip half, limit 100" % route: ""})
+            for header in headers:
+                if header in group:
+                    f.write("%s;"%group[header])
+                else:
+                    f.write("_%s_;"%sl.DEFAULT_FILL_AMT)
+
+            f.write("\n")
+
--- test/load/sample_local.py
+++ test/load/sample_local.py
@@ -3,16 +3,18 @@
 # Number of docs to insert:
 to_fill = {
         "person" : 10000,
-        "group_member" : 1000000,
+        "group_member" : 1000,
         "group" : 10,
         "group_role" : 10,
-        "person_ss" : 100000,
-        "person_hs" : 100000,
-        "ss_review" : 200110,
-        "hs_review" : 200110,
+        "person_ss" : 10000,
+        "person_hs" : 10000,
+        "ss_review" : 20010,
+        "hs_review" : 20010,
         "specialization_review" : 11,
-        "person_specialization": 20011,
+        "person_specialization": 2001,
 }
+# default number of docs (used if not present in to_fill
+DEFAULT_FILL_AMT = 10
 # Offer to validate each inserted doc against pymodm validation rules
 validate_after_fill = False
 # Fields that must be as diverse as possible when generating unique multi-reference combos
@@ -36,12 +38,21 @@ uri="mongodb://localhost:27017"
 client = MongoClient(uri)
 db = client[db_name]
 
-colList = db.list_collection_names()
-for col in colList:
-    db.drop_collection(col)
-    #db[col].delete_many({})
-    print("dropped collection " + col)
-print("done")
+def drop_collections(db):
+    colList = db.list_collection_names()
+    for col in colList:
+        db.drop_collection(col)
+        #db[col].delete_many({})
+        print("dropped collection " + col)
+    print("done")
+
+def clear_collections(db):
+    colList = db.list_collection_names()
+    for col in colList:
+        #db.drop_collection(col)
+        db[col].delete_many({})
+        print("cleared collection " + col)
+    print("done")
 
 
 import data.reviewer_model as model
@@ -84,10 +95,10 @@ ignore_list = [
     ]
 
 
-filled={}
-starting_ids = {}
-doc_fields = {}
+
+
 doc_u_indexes = {}
+doc_fields = {}
 
 members = inspect.getmembers(model, inspect.isclass)
 doc_class_list = []
@@ -138,7 +149,7 @@ for doc_class in doc_class_list:
     if len(unique_indexes) > 0:
         doc_u_indexes.update({col_name: unique_indexes})
 
-doc_ctr = 1
+
 
 def gen_ref(num : int, refs : list):
     n = num
@@ -157,174 +168,183 @@ def gen_ref(num : int, refs : list):
     out_list.append(n)
     return out_list
 
-remaining_fields = doc_fields.copy()
 one_pass_for_everyone = hash_password("12345")
 
-all_group_roles = []
+def fill_db(db, to_fill, validate_after_fill, diverse):
 
-while len(remaining_fields) > 0:
-    print("--------------")
-    for col_name, field_list in doc_fields.items():
-        print("------")
-        print(datetime.datetime.now())
-        can_fill = True
-        if col_name in filled:
-            print(col_name + " is already filled, skipping")
-            can_fill = False
-        for field, info in field_list.items():
-            if info["ref"] and info["ref"] not in filled:
-                print("%s not filled yet, skipping %s for now" %(info["ref"],col_name))
+    filled = {}
+    starting_ids = {}
+
+    doc_ctr = 1
+    remaining_fields = doc_fields.copy()
+    all_group_roles = []
+
+    while len(remaining_fields) > 0:
+        print("--------------")
+        for col_name, field_list in doc_fields.items():
+            print("------")
+            print(datetime.datetime.now())
+            can_fill = True
+            if col_name in filled:
+                print(col_name + " is already filled, skipping")
                 can_fill = False
-        if can_fill:
-            ref_count = {}
-            print("filling " + col_name)
-            if col_name in to_fill:
-                insert_amount = to_fill[col_name]
-            else:
-                insert_amount = 10
-            increment = 1
-            if col_name in doc_u_indexes:
-                print("Need to guarantee reference combinations uniqueness for %s"%col_name)
-                max_comb = 1
+            for field, info in field_list.items():
+                if info["ref"] and info["ref"] not in filled:
+                    print("%s not filled yet, skipping %s for now" %(info["ref"],col_name))
+                    can_fill = False
+            if can_fill:
+                ref_count = {}
+                print("filling " + col_name)
+                if col_name in to_fill:
+                    insert_amount = to_fill[col_name]
+                else:
+                    insert_amount = DEFAULT_FILL_AMT
+                increment = 1
+                if col_name in doc_u_indexes:
+                    print("Need to guarantee reference combinations uniqueness for %s"%col_name)
+                    max_comb = 1
 
-                for field, info in field_list.items():
-                    if field in doc_u_indexes[col_name]:
-                        max_comb *= filled[info["ref"]]
-                        ref_count.update({info["ref"] : filled[info["ref"]]})
-                print("Maximum reference combinations: %s" % max_comb)
-                print("Requested reference combinations: %s" % insert_amount)
-                if max_comb < insert_amount:
-                    raise ValueError("uniqueness is impossible for %s" %col_name)
-                print(ref_count)
-                increment = int(max_comb/insert_amount)
-            doc_list = []
-            cur_starting_id = doc_ctr
-            starting_ids.update({col_name : cur_starting_id})
-            added_cnt = 0
+                    for field, info in field_list.items():
+                        if field in doc_u_indexes[col_name]:
+                            max_comb *= filled[info["ref"]]
+                            ref_count.update({info["ref"] : filled[info["ref"]]})
+                    print("Maximum reference combinations: %s" % max_comb)
+                    print("Requested reference combinations: %s" % insert_amount)
+                    if max_comb < insert_amount:
+                        raise ValueError("uniqueness is impossible for %s" %col_name)
+                    print(ref_count)
+                    increment = int(max_comb/insert_amount)
+                doc_list = []
+                cur_starting_id = doc_ctr
+                starting_ids.update({col_name : cur_starting_id})
+                added_cnt = 0
 
-            r_col_names = []
-            r_cnts = []
-            r_field_names = []
-            for r_name, r_cnt in ref_count.items():
-                r_col_names.append(r_name)
-                r_cnts.append(r_cnt)
-                for field, info in field_list.items():
-                    if info["ref"] == r_name:
-                        r_field_names.append(field)
-            #diversify
-            for item in diverse:
-                if item in r_field_names:
-                    ind = r_field_names.index(item)
-                    if ind > 0:
-                        r_col_names[0], r_col_names[ind] = r_col_names[ind], r_col_names[0]
-                        r_cnts[0], r_cnts[ind] = r_cnts[ind], r_cnts[0]
-                        r_field_names[0], r_field_names[ind] = r_field_names[ind], r_field_names[0]
-                    break
-            auth_list = []
-            for i in range(insert_amount):
-                cur_doc = {"_id" : int_to_obj_id(doc_ctr)}
-                #first we are going to fill reference fields that are in unique index
-                if ref_count:
-                    relative_num = increment *(doc_ctr - cur_starting_id)
-                    ref_field_vals = gen_ref(relative_num, r_cnts)
-                    for i, r_name in enumerate(r_field_names):
-                        start = starting_ids[r_col_names[i]]
-                        cur_doc.update({r_field_names[i]: int_to_obj_id(start + ref_field_vals[i])})
-                for field, info in field_list.items():
-                    if info["type"] == "string":
-                        if field == "phone_no":
-                            cur_doc.update({field: "8901"+ "%07d"%(doc_ctr)})
-                        else:
-                            cur_doc.update({field : field + "_" + str(random.randint(0,999)) +
-                                                                     "_" + str(doc_ctr)})
-                    elif info["type"] == "date":
-                        cur_doc.update({field: datetime.datetime(random.randrange(1900, 2000),
-                                             random.randrange(1, 12),
-                                             random.randrange(1, 28))})
-                    elif info["type"] == "double":
-                        cur_doc.update({field: random.random()*100})
-                    elif info["type"] == "int":
-                        cur_doc.update({field: random.randint(0,10)})
-                    elif info["type"] == "bool":
-                        cur_doc.update({field: True})
-                    elif info["type"] == "timestamp":
-                        cur_doc.update({field: None})
-                    elif info["type"] == "list":
-                        cur_doc.update({field: ["stub"]})
-                    elif info["type"] == "dict":
-                        cur_doc.update({field: {"1" : "stub"}})
-                    elif info["type"] == "binData":
-                        pass
-                    elif info["type"] == "ref_list":
-                        if col_name == "group" and field == "role_list":
-                            cur_doc.update({"role_list" : all_group_roles})
-                    elif info["type"] == "objectId":
-                        if info["ref"] not in ref_count:
-                            start = starting_ids[info["ref"]]
-                            amt = filled[info["ref"]]
-                            num = start + random.randint(0,amt-1)
-                            cur_doc.update({field: int_to_obj_id(num)})
-                doc_list.append(cur_doc)
+                r_col_names = []
+                r_cnts = []
+                r_field_names = []
+                for r_name, r_cnt in ref_count.items():
+                    r_col_names.append(r_name)
+                    r_cnts.append(r_cnt)
+                    for field, info in field_list.items():
+                        if info["ref"] == r_name:
+                            r_field_names.append(field)
+                #diversify
+                for item in diverse:
+                    if item in r_field_names:
+                        ind = r_field_names.index(item)
+                        if ind > 0:
+                            r_col_names[0], r_col_names[ind] = r_col_names[ind], r_col_names[0]
+                            r_cnts[0], r_cnts[ind] = r_cnts[ind], r_cnts[0]
+                            r_field_names[0], r_field_names[ind] = r_field_names[ind], r_field_names[0]
+                        break
+                auth_list = []
+                for i in range(insert_amount):
+                    cur_doc = {"_id" : int_to_obj_id(doc_ctr)}
+                    #first we are going to fill reference fields that are in unique index
+                    if ref_count:
+                        relative_num = increment *(doc_ctr - cur_starting_id)
+                        ref_field_vals = gen_ref(relative_num, r_cnts)
+                        for i, r_name in enumerate(r_field_names):
+                            start = starting_ids[r_col_names[i]]
+                            cur_doc.update({r_field_names[i]: int_to_obj_id(start + ref_field_vals[i])})
+                    for field, info in field_list.items():
+                        if info["type"] == "string":
+                            if field == "phone_no":
+                                cur_doc.update({field: "8901"+ "%07d"%(doc_ctr)})
+                            else:
+                                cur_doc.update({field : field + "_" + str(random.randint(0,999)) +
+                                                                         "_" + str(doc_ctr)})
+                        elif info["type"] == "date":
+                            cur_doc.update({field: datetime.datetime(random.randrange(1900, 2000),
+                                                 random.randrange(1, 12),
+                                                 random.randrange(1, 28))})
+                        elif info["type"] == "double":
+                            cur_doc.update({field: random.random()*100})
+                        elif info["type"] == "int":
+                            cur_doc.update({field: random.randint(0,10)})
+                        elif info["type"] == "bool":
+                            cur_doc.update({field: True})
+                        elif info["type"] == "timestamp":
+                            cur_doc.update({field: None})
+                        elif info["type"] == "list":
+                            cur_doc.update({field: ["stub"]})
+                        elif info["type"] == "dict":
+                            cur_doc.update({field: {"1" : "stub"}})
+                        elif info["type"] == "binData":
+                            pass
+                        elif info["type"] == "ref_list":
+                            if col_name == "group" and field == "role_list":
+                                cur_doc.update({"role_list" : all_group_roles})
+                        elif info["type"] == "objectId":
+                            if info["ref"] not in ref_count:
+                                start = starting_ids[info["ref"]]
+                                amt = filled[info["ref"]]
+                                num = start + random.randint(0,amt-1)
+                                cur_doc.update({field: int_to_obj_id(num)})
+                    doc_list.append(cur_doc)
 
-                if col_name == "person":
-                    auth_doc = {"_id": int_to_obj_id(doc_ctr + \
-                                (10 if not ("person" in to_fill) else to_fill["person"]))}
-                    auth_doc.update({
-                        "attempts": 0,
-                        "auth_code": None,
-                        "is_approved": True,
-                        "last_send_time": datetime.datetime.now(datetime.timezone.utc),
-                        "password": one_pass_for_everyone,
-                        "permissions": 0,
-                        "person_id": cur_doc["_id"],
-                        "phone_no" : cur_doc["phone_no"],
-                        "session_id" : None
-                    })
-                    auth_list.append(auth_doc)
-                doc_ctr += 1
+                    if col_name == "person":
+                        auth_doc = {"_id": int_to_obj_id(doc_ctr + \
+                                    (DEFAULT_FILL_AMT if not ("person" in to_fill) else to_fill["person"]))}
+                        auth_doc.update({
+                            "attempts": 0,
+                            "auth_code": None,
+                            "is_approved": True,
+                            "last_send_time": datetime.datetime.now(datetime.timezone.utc),
+                            "password": one_pass_for_everyone,
+                            "permissions": 0,
+                            "person_id": cur_doc["_id"],
+                            "phone_no" : cur_doc["phone_no"],
+                            "session_id" : None
+                        })
+                        auth_list.append(auth_doc)
+                    doc_ctr += 1
 
-            inserted_ids = db[col_name].insert_many(doc_list, ordered=False).inserted_ids
-            #all groups will have all roles possible to avoid validation issues
-            if col_name == "group_role":
-                all_group_roles = inserted_ids
-            cnt = len(inserted_ids)
-            filled.update({col_name: cnt})
-            if auth_list:
-                a_cnt = len(db["auth_info"].insert_many(auth_list, ordered=False).inserted_ids)
-                doc_ctr += a_cnt
+                inserted_ids = db[col_name].insert_many(doc_list, ordered=False).inserted_ids
+                #all groups will have all roles possible to avoid validation issues
+                if col_name == "group_role":
+                    all_group_roles = inserted_ids
+                cnt = len(inserted_ids)
+                filled.update({col_name: cnt})
+                if auth_list:
+                    a_cnt = len(db["auth_info"].insert_many(auth_list, ordered=False).inserted_ids)
+                    doc_ctr += a_cnt
 
-            remaining_fields.pop(col_name)
+                remaining_fields.pop(col_name)
 
-db["service"].insert_one({"_id" : int_to_obj_id(doc_ctr),
-                          "db_version" : "0.4",
-                          "api_version" : constants.api_version})
-doc_ctr+= 1
-db["auth_info"].insert_one({"_id" : int_to_obj_id(doc_ctr),
-                          "attempts": 0,
-                        "auth_code": None,
-                        "is_approved": True,
-                        "last_send_time": None,
-                        "password": hash_password("SomeSecurePass"),
-                        "permissions": 1,
-                        "person_id": None,
-                        "phone_no" : "79032233223",
-                        "session_id" : None})
-doc_ctr+= 1
+    db["service"].insert_one({"_id" : int_to_obj_id(doc_ctr),
+                              "db_version" : "0.4",
+                              "api_version" : constants.api_version})
+    doc_ctr+= 1
+    db["auth_info"].insert_one({"_id" : int_to_obj_id(doc_ctr),
+                              "attempts": 0,
+                            "auth_code": None,
+                            "is_approved": True,
+                            "last_send_time": None,
+                            "password": hash_password("SomeSecurePass"),
+                            "permissions": 1,
+                            "person_id": None,
+                            "phone_no" : "79032233223",
+                            "session_id" : None})
+    doc_ctr+= 1
 
-print("--------------------------")
-print("total inserted:")
-for key,item in filled.items():
-    print("%s: %s"% (key,item))
+    print("--------------------------")
+    print("total inserted:")
+    for key,item in filled.items():
+        print("%s: %s"% (key,item))
 
 
-if validate_after_fill:
-    if True:    #sys.platform == "win32":
-        val = input("Validate docs? Type something to validate or press return to skip\n\n")
-        if val:
-            for doc_class in doc_class_list:
-                if doc_class.__name__ not in ignore_list:
-                    print("validating %s..." % doc_class.__name__, end="", flush=True)
-                    for obj in doc_class.objects.all():
-                        obj.clean()
-                    print(" done")
+    if validate_after_fill:
+        if True:    #sys.platform == "win32":
+            val = input("Validate docs? Type something to validate or press return to skip\n\n")
+            if val:
+                for doc_class in doc_class_list:
+                    if doc_class.__name__ not in ignore_list:
+                        print("validating %s..." % doc_class.__name__, end="", flush=True)
+                        for obj in doc_class.objects.all():
+                            obj.clean()
+                        print(" done")
 
+if __name__ == "__main__":
+    drop_collections(db)
+    fill_db(db,to_fill,validate_after_fill,diverse)
