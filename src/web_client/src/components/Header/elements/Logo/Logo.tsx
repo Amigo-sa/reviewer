4e2c681913c33dae1ae5ffa1227e940f80138fb3
ZhilinRodion <zhilin.rodion@gmail.com>
2018-10-18 22:07:46 +0300

    Moved css definition to tsx file for Logo component.

--- src/web_client/src/components/Header/elements/Logo/Logo.tsx
+++ src/web_client/src/components/Header/elements/Logo/Logo.tsx
@@ -1,23 +1,50 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { Theme, createStyles, withStyles, WithStyles } from "@material-ui/core";
 
-import "./Logo.css";
-interface IProps {
+const styles = (theme: Theme) =>
+    createStyles({
+        link: {
+            display: "flex",
+            flex: "1 1 auto",
+            textDecoration: "none",
+        },
+        img: {
+            width: "96px",
+            height: "96px",
+        },
+        titleSection: {
+            height: "64px",
+        },
+        title: {
+            display: "block",
+            fontSize: "30px",
+            color: "#000000",
+        },
+        subtitle: {
+            display: "block",
+            fontSize: "20px",
+            color: "#02588A",
+        },
+    });
+
+interface IProps extends WithStyles<typeof styles> {
     title?: string;
 }
 
 class Logo extends React.Component<IProps> {
     public render() {
         const { title } = this.props;
+        const { classes } = this.props;
         return (
-            <Link to="/" className={"logo__link"} title={title}>
-                <img src="img/logo.png" alt="logo" className={"logo__img"} />
-                <div className={"logo__title"}>
-                    <span className={"logo__title-main"}>Skill for life</span>
-                    <span className={"logo__title-sec"}>reviewer</span>
+            <Link to="/" className={classes.link} title={title}>
+                <img src="static/img/logo.png" alt="logo" className={classes.img} />
+                <div className={classes.titleSection}>
+                    <span className={classes.title}>Skill for life</span>
+                    <span className={classes.subtitle}>reviewer</span>
                 </div>
             </Link>
         );
     }
 }
-export default Logo;
+export default withStyles(styles)(Logo);
