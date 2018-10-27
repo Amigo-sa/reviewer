0eb0828beeb5d09106b051c33c192aa3f101905f
Ivan Popandopulo <idpopandopulo@yandex.ru>
2018-10-27 11:54:03 +0300

    web_client add length parameter for responce

--- src/web_client/src/server-api/persons/FindPersonsResponse.ts
+++ src/web_client/src/server-api/persons/FindPersonsResponse.ts
@@ -2,5 +2,6 @@ import Person from "./Person";
 import Response from "src/server-api/Response";
 
 export default class FindPersonsResponse extends Response {
+    public length: number;
     public list?: Person[];
 }
