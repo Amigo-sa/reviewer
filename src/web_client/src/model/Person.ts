2cfa6faf6902e6da27b4a28f9d50f10300931095
ZhilinRodion <zhilin.rodion@gmail.com>
2018-11-18 13:28:24 +0300

    Added initial version of model folder and converters.

---
+++ src/web_client/src/model/Person.ts
@@ -0,0 +1,13 @@
+export default class Person {
+    public id: string;
+    public firstName: string;
+    public middleName: string;
+    public surname: string;
+    public birthDate?: string;
+    public phone?: string;
+    public organizationName: string;
+    public isHaveAvatar: boolean;
+    public departmentName?: string;
+    public specializationDisplayText?: string;
+    public notes?: string;
+}
